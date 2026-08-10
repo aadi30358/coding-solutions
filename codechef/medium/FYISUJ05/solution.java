cc1939b73ba11a7c341c5c05a88630a8ac01117d
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-10 10:27:55 +0530

    ✅ FYISUJ05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ codechef/medium/FYISUJ05/solution.java
@@ -0,0 +1,7 @@
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+        int heroname = 18;
+        System.out.println("The value stored in variable is:" + heroname);
+    }
+}
