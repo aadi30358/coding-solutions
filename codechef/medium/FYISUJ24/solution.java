ea9f036e1ac02fa27180c4a09008e2ad457c130a
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:01:34 +0530

    ✅ FYISUJ24 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ24/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        long totalSignalsReceived = 9876543210L;
+        
+        System.out.println("Total Signals Received: " + totalSignalsReceived);
+    }
+}
