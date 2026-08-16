d068f557e536841bd7baf718d791e2a671c3b105
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:52:26 +0530

    ✅ FYISUJ45 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ45/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        double reach = 3452.78;
+        
+        boolean veh = true;
+        
+        System.out.println("Depth Reached: " + reach);
+        System.out.println("Vechicle Exploring: " + veh);
+        
+
+    }
+}
