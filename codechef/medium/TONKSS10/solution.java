7544e63be1cf8b068cbc75da3a1a0df3239d8ea3
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 19:27:14 +0530

    ✅ TONKSS10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/TONKSS10/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        long dist = 450;
+        double convertedDistance = dist;
+        boolean active = true;
+        
+        System.out.println("Distance in Long: " + dist);
+        System.out.println("Converted Distance in Double: " + convertedDistance + "km");
+        System.out.println("Is Journey Active: " + active);
+    }
+}
