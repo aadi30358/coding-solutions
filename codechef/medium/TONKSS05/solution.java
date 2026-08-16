c8912bb10f6cac5f1c002a6e8ae84ecfc1f24014
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 19:16:53 +0530

    ✅ TONKSS05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/TONKSS05/solution.java
@@ -0,0 +1,18 @@
+class Codechef {
+    public static void main(String[] args) {
+        int goal = 3;
+        int mode = 2;
+        int shot =5;
+        
+        double player1 = 8.5;
+        double player2 = 7.9;
+        
+        System.out.println("Goals Scored: " + goal);
+        System.out.println("Assists Made: " + mode);
+        
+        System.out.println("Shots on Target: " + shot);
+        System.out.println("Player 1 Rating: " + player1);
+        System.out.println("Player 2 Rating: " + player2);
+        
+    }
+}
