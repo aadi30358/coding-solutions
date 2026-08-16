e98a91f8236db059a3152a9b5a0ecc46bfae6e10
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 19:20:47 +0530

    ✅ TONKSS08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/TONKSS08/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        int age = 25;
+        double convertedAge = age;
+        
+        System.out.println("Integer Age: " + age);
+        System.out.println("Converted Age (Double): " + convertedAge);
+    }
+}
