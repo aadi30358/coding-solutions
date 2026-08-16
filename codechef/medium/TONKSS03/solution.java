a359ea5968924252795aa4cf4173c02a1d069f10
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 19:10:29 +0530

    ✅ TONKSS03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/TONKSS03/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declaring multiple int variables in one statement
+        int mathScore = 85, scienceScore = 90, englishScore = 78;
+
+        // Printing the values of all variables
+        System.out.println("Math Score: " + mathScore);
+        System.out.println("Science Score: " + scienceScore);
+        System.out.println("English Score: " + englishScore);
+    }
+}
