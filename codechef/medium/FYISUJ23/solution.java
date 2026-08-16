b0553e5f32c798d5b5f891b6c4ac0d4584657102
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 17:59:15 +0530

    ✅ FYISUJ23 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

--- codechef/medium/FYISUJ23/solution.java
+++ codechef/medium/FYISUJ23/solution.java
@@ -1,9 +1,8 @@
 class Codechef {
     public static void main(String[] args) {
-        // Declare and assign a large number using long
-        long totalSignalsReceived = 9_876_543_210L;  // Total signals from space missions
-
-        // Print the stored value
+        
+        long totalSignalsReceived = 9876543210L;
+        
         System.out.println("Total Signals Received: " + totalSignalsReceived);
     }
 }
