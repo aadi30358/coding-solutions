fa4796063410b612bd3abf4987a38e3f5541b0be
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:14:40 +0530

    ✅ FYISUJ29 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ29/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        float currentRating = 4.7f;
+        float maxRating = 5.0f;
+        
+        System.out.println("Current Video Rating: " + currentRating);
+        System.out.println("Maximum Possible Rating: " + maxRating);
+        
+    }
+}
