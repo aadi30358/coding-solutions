6a4c2f85064259e770d1e3182b69630ee4be8983
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:38:23 +0530

    ✅ FYISUJ35 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ35/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        double temp = 28.3;
+        double level = 125.6789;
+        
+        System.out.println("Temperature: " + temp + "°C");
+        System.out.println("Rainfall Level: " + level + "mm");
+    }
+}
