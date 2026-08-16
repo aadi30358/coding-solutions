c2e16b37e1c07a59dd76f12a5442680b5fdfc0cb
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:45:45 +0530

    ✅ FYISUJ40 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ40/solution.java
@@ -0,0 +1,22 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign mission details
+        
+        
+        char code = 'A';
+        float size = 7f;
+        long travel = 30000;
+        
+        System.out.println("Mission Code: " + code);
+        System.out.println("Crew Size: " + size);
+        System.out.println("Distance Traveled: " + travel + "km");
+
+
+
+
+        // Print the mission data
+
+
+
+    }
+}
