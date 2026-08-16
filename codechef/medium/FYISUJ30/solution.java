d5caa61edae86051571c726b2d7330d9b47d5712
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:29:00 +0530

    ✅ FYISUJ30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ30/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign values
+        float fuelEffiency = 18.5f;
+        float distanceCovered = 150f;
+        
+        System.out.println("Fuel Efficiency: " + fuelEffiency + "km/l");
+        System.out.println("Distance Covered: " + distanceCovered + "km");
+
+
+
+        //  Print the values
+
+
+    }
+}
