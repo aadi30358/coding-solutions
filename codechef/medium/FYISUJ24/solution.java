4c25db4c08dd7c2b05b7e21afc999b2a61b69a6e
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:03:51 +0530

    ✅ FYISUJ24 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

--- codechef/medium/FYISUJ24/solution.java
+++ codechef/medium/FYISUJ24/solution.java
@@ -1,8 +1,10 @@
 class Codechef {
     public static void main(String[] args) {
         
-        long totalSignalsReceived = 9876543210L;
-        
-        System.out.println("Total Signals Received: " + totalSignalsReceived);
+    long speedOfLight = 299792458;
+    long secondsInYear = 31536000;
+    
+    System.out.println("Speed of Light: " + speedOfLight + " m/s");
+    System.out.println("Seconds in a Year: " + secondsInYear);
     }
 }
