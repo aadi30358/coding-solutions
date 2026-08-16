f2b3134e540fffa38627459784d3994931d556c9
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-16 18:10:16 +0530

    ✅ FYISUJ28 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ codechef/medium/FYISUJ28/solution.java
@@ -0,0 +1,7 @@
+class Codechef {
+    public static void main(String[] args) {
+        float averagaeTemperature = 28.75f;
+        
+        System.out.println("Average Temperature: " + averagaeTemperature +"°C");
+    }
+}
