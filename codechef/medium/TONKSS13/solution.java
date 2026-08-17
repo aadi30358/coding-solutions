5acb6a29b86795c30e9c1bda42b5e5ff86967a0c
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-17 10:00:51 +0530

    ✅ TONKSS13 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/TONKSS13/solution.java
+++ codechef/medium/TONKSS13/solution.java
@@ -1,10 +1,11 @@
 class Codechef {
     public static void main(String[] args) {
-        long employeeId = 12345;
+        long employeeId = 1234567890L;
         
         int convertedId = (int) employeeId;
         
         System.out.println("Employee ID (Long): " + employeeId);
-        System.out.println("")
+        System.out.println("Converted Employee ID (Int): " + convertedId);
+        
     }
 }
