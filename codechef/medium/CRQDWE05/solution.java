180c600c9a36910adf5e2845cb16c65691481c5b
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-17 19:36:28 +0530

    ✅ CRQDWE05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/CRQDWE05/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Write your code here
+        int pushups = 40, situps = 30, squats = 20;
+        
+        int totalExerices = pushups + situps + squats;
+        
+        System.out.println(totalExerices);
+        
+        
+    }
+}
