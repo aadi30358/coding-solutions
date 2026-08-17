5a7bc279b3656f00ab147ce102570c7397a7beea
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-17 19:41:23 +0530

    ✅ CRQDWE10 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/CRQDWE10/solution.cpp
@@ -0,0 +1,11 @@
+class CodeChef {
+    public static void main(String[] args) {
+        
+        int num1 =30;
+        int num2 = 10;
+        
+        int difference = num1 - num2;
+        
+        System.out.println("Difference: " + difference);
+}
+}
