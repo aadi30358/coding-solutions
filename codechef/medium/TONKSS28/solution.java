5aaef267c129bd55077dbb1d6d9ff769bb9288c8
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-08-17 10:50:49 +0530

    ✅ TONKSS28 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- codechef/medium/TONKSS28/solution.java
+++ codechef/medium/TONKSS28/solution.java
@@ -1,11 +1,7 @@
 class Codechef {
     public static void main(String[] args) {
+        final float MAX_SPEED = 0.18f;
         
-        // Declaring and assigning a fixed tax rate
-        float FIXED_RATE = 0.18f;
-
-        // Printing the Fixed Tax Rate
-        System.out.println("Fixed Tax Rate: " + FIXED_RATE);
-
+        System.out.println("")
     }
 }
