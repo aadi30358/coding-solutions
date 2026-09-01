04ad087eb0d48cd6915e8ec9c5058868805c8fab
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-01 22:14:37 +0530

    ✅ QVQICP30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/QVQICP30/solution.java
@@ -0,0 +1,6 @@
+class Codechef {
+    public static void main(String[] args) {
+        System.out.println("My name is John.:  + My favorite color is Blue.My favorite hobby is Reading.");
+    }
+    
+}
