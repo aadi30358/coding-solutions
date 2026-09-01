4fee7477970434aefe7937d7092e17b346692211
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-01 22:24:25 +0530

    ✅ OBZPOG18 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/OBZPOG18/solution.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        System.out.println('A');
+        System.out.println("John");
+        System.out.println(true);
+        
+    }
+}
+
