e742d75ab28bb5f44439e697946f01658a09ab06
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:44:42 +0530

    ✅ FYISUJ39 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ39/solution.cpp
+++ codechef/medium/FYISUJ39/solution.cpp
@@ -1,7 +1,9 @@
 class Codechef {
     public static void main(String[] args) {
-        char grade = 'A';
+        float temp = 28.3F;
+        double level = 125.6789;
         
-        System.out.println("Student Grade: " + grade);
+        System.out.println("Temperature: " + temp + "°C");
+        System.out.println("Rainfall Level: " + level + "mm");
     }
 }
