b4626b66cfc1a9da4ecbf486c2524d520ce27af7
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:36:47 +0530

    ✅ FYISUJ25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ25/solution.java
+++ codechef/medium/FYISUJ25/solution.java
@@ -1,7 +1,7 @@
 class Codechef {
     public static void main(String[] args) {
-        long transactionID = 5789000;
-        long branchCode = 3052;
+      long transactionID = 5123456789000L;
+      long branchCode = 3052;
         
         System.out.println("Transaction ID: " + transactionID);
         System.out.println("Branch Code: " + branchCode);
