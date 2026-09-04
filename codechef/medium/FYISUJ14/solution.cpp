4905415cdb161e97e225fc02dace19a5ef6c7e5c
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:28:28 +0530

    ✅ FYISUJ14 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ14/solution.cpp
+++ codechef/medium/FYISUJ14/solution.cpp
@@ -1,9 +1,16 @@
-class codechef {
+class Codechef {
     public static void main(String[] args) {
-        byte hrt =45;
-        byte crt = -20;
-        
-        System.out.println("Hot Region Temperature: " + hrt);
-        System.out.println("Cold Region Temperature: " + crt);
-    } 
+      
+        int cp = 5_00_000;
+        short dp = 25_000;
+      
+        System.out.println("City Population:" + cp);
+        System.out.println("District Population:" + dp);
+
+
+
+        // Print the population values
+
+
+    }
 }
