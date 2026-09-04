7850b6726d1873df65725e053307db461c762b31
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:22:40 +0530

    ✅ FYISUJ15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ15/solution.java
+++ codechef/medium/FYISUJ15/solution.java
@@ -1,10 +1,8 @@
 class Codechef {
     public static void main(String[] args) {
-        //  Declare and assign earthquake intensity readings
-        short regionOneIntensity = 12000;
-        short regionTwoIntensity = -5000;
-        byte regionThreeIntensity = 0;
-        
+       short regionOneIntensity = 12000;
+       short regionTwoIntensity = -5000;
+       byte  regionThreeIntensity = 0;
         
         
         System.out.println("Region One Intensity: " + regionOneIntensity);
