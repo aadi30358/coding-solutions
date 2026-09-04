e37897af1e1a5bcf9f721b4052a3a33c8cd10146
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:24:23 +0530

    ✅ FYISUJ20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ20/solution.java
+++ codechef/medium/FYISUJ20/solution.java
@@ -1,11 +1,11 @@
 class Codechef {
     public static void main(String[] args) {
-        // Declare and assign population values
-        int cityPopulation = 5_000_000;
-        int districtPopulation = 25_000;
-        
-        System.out.println("City Population:" + cityPopulation);
-        System.out.println("District Population:" + districtPopulation);
+      
+        int cp = 5_00_000;
+        short dp = 25_000;
+      
+        System.out.println("City Population:" + cp);
+        System.out.println("District Population:" + dp);
 
 
 
