bf8fa53856071f4feda93ac339aeeabbf53121c9
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:20:39 +0530

    ✅ FYISUJ11 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ codechef/medium/FYISUJ11/solution.java
@@ -0,0 +1,9 @@
+class codechef {
+    public static void main(String[] args) {
+        byte hrt =45;
+        byte crt = -20;
+        
+        System.out.println("Hot Region Temperature: " + hrt);
+        System.out.println("Cold Region Temperature: " + crt);
+    } 
+}
