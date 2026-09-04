68574ca167f4fd55b060d93d44e6273f58cf306a
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:20:37 +0530

    ✅ FYISUJ10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ10/solution.java
+++ codechef/medium/FYISUJ10/solution.java
@@ -1,15 +1,9 @@
-class Codechef {
+class codechef {
     public static void main(String[] args) {
-        //  Declare and assign temperature readings
-        byte htr = 45;
+        byte hrt =45;
         byte crt = -20;
-
-
-
-        //  Print the temperature readings
-        System.out.println("Hot Region Temperature:" + htr);
-        System.out.println("Cold Region Temperature:" + crt);
-
-
-    }
+        
+        System.out.println("Hot Region Temperature: " + hrt);
+        System.out.println("Cold Region Temperature: " + crt);
+    } 
 }
