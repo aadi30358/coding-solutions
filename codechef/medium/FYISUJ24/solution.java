b1f235cb57f578f64d092731c8ed0dcae9734252
G.Yaswanth Adithya <2400030358@kluniversity.in>
2026-09-04 12:35:53 +0530

    ✅ FYISUJ24 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/medium/FYISUJ24/solution.java
+++ codechef/medium/FYISUJ24/solution.java
@@ -1,8 +1,7 @@
 class Codechef {
     public static void main(String[] args) {
-        
-    long speedOfLight = 299792458;
-    long secondsInYear = 31536000;
+   long speedOfLight = 299792458;
+   long secondsInYear = 31536000;
     
     System.out.println("Speed of Light: " + speedOfLight + " m/s");
     System.out.println("Seconds in a Year: " + secondsInYear);
